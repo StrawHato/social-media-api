88b6efeca623c95f6b045186a24c29f9c1d58765
Maksym Pakula <gargantadoto@gmail.com>
2026-03-18 19:54:54 +0200

    Add Users viewset and endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,14 +1,22 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+from rest_framework import routers
 
-from user.views import CreateUserView
+from user.views import (
+    CreateUserView,
+    UserViewSet
+)
 
+
+router = routers.SimpleRouter()
+router.register("users", UserViewSet)
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("registration/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
--- user/views.py
+++ user/views.py
@@ -1,7 +1,13 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import generics, viewsets
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
