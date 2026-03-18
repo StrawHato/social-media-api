fbf88b832091cbbdb3f7d94ae0050201d1831e1b
Maksym Pakula <gargantadoto@gmail.com>
2026-03-18 19:03:06 +0200

    Refactor User serializer

--- user/serializers.py
+++ user/serializers.py
@@ -7,7 +7,8 @@ from user.models import Follow
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "first_name", "last_name", "followers", "following", "password")
+        fields = ("id", "email", "first_name", "last_name", "is_staff", "password")
+        read_only_fields = ("id", "is_staff")
         extra_kwargs = {
             "password": {
                 "write_only": True,
