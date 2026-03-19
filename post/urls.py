1c2eee89629a0783b0c2d97a68d82d0575bb22bf
Maksym Pakula <gargantadoto@gmail.com>
2026-03-19 20:40:16 +0200

    Add Post viewset and routes

--- post/urls.py
+++ post/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from post.views import HashtagViewSet
+from post.views import HashtagViewSet, PostViewSet
 
 
 router = routers.DefaultRouter()
 router.register("hashtags", HashtagViewSet)
+router.register("posts", PostViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- post/views.py
+++ post/views.py
@@ -8,3 +8,47 @@ from post.serializers import HashtagSerializer, PostSerializer, PostListDetailSe
 class HashtagViewSet(viewsets.ModelViewSet):
     queryset = Hashtag.objects.all()
     serializer_class = HashtagSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Post.objects.select_related("owner")
+        .prefetch_related("hashtags")
+        .annotate(
+            likes_count=Count("likes"),
+            comments_count=Count("comments"),
+        )
+    )
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        followings = self.request.user.following.all()
+        user_ids = [f.following.id for f in followings] + [self.request.user.id]
+        queryset = self.queryset.filter(owner__id__in=user_ids)
+
+        hashtags = self.request.query_params.get("hashtags")
+        author = self.request.query_params.get("author")
+        content = self.request.query_params.get("content")
+
+        if author:
+            queryset = queryset.filter(
+                owner__username=author,
+            )
+        if content:
+            queryset = queryset.filter(
+                content__icontains=content,
+            )
+        if hashtags:
+            queryset = queryset.filter(
+                hashtags__id__in=[int(hashtag) for hashtag in hashtags.split(",")],
+            )
+        return queryset
+
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return PostListDetailSerializer
+        return PostSerializer
