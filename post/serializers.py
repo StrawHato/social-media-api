27a89377ea9e039327b585c6e9b9323c2426a35b
Maksym Pakula <gargantadoto@gmail.com>
2026-03-19 20:28:35 +0200

    Add Post serializer

--- post/serializers.py
+++ post/serializers.py
@@ -17,3 +17,9 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ("id", "content", "owner")
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ("id", "content", "owner", "picture", "hashtags")
+        read_only_fields = ("id", "owner",)
